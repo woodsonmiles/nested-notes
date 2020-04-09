fe73fabf1d62f0a21bc3b5bea99c6f8b7cc22fba
Woodson Miles <woodson.miles@my.wheaton.edu>
2020-04-09 15:40:33 +0000

    Added github url to setup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Terminal note-taking application",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/woodsonmiles/nested-notes",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
