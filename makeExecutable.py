27d93cfd9f885404f3395ea99f800929c2d54f3f
woodsonmiles <woodson.miles@my.wheaton.edu>
2020-04-14 10:32:48 -0400

    makeExecutable.py seems functional

--- makeExecutable.py
+++ makeExecutable.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 import PyInstaller.__main__
+from os import path
 
 PyInstaller.__main__.run([
     '--name=nestingnote',
     '--onefile',
     '--console',
     '--icon=nestingdoll.ico',
-    os.path.join('nestingnote', '__main__.py'),
+    path.join('nestingnote', '__main__.py'),
+])
