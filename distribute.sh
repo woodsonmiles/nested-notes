6fb4c6fd4eeb9859a469f8393f65799cbb44eb14
woodsonmiles <woodson.miles@my.wheaton.edu>
2020-04-14 13:23:39 -0400

    default file added to main

--- distribute.sh
+++ distribute.sh
@@ -2,6 +2,7 @@
 rm -rf build/
 rm -rf dist/
 rm -rf nestingnote.egg-info/
+rm -f nestingnote.spec
 python3 setup.py sdist bdist_wheel
 python3 -m twine upload dist/*
 
--- nestingnote/__main__.py
+++ nestingnote/__main__.py
@@ -3,18 +3,37 @@ from nestingnote.model import Model
 from nestingnote.linuxView import LinuxView
 from curses import wrapper
 from nestingnote.controller import Controller
-from sys import argv
+import sys
+import os
+from pathlib import Path
 
 
 def main(window):
-    try:
-        file_name = argv[1]
-    except IndexError:
-        raise Exception("Missing first argument: file name")
+    file_path = get_file_path()
     view = LinuxView(window)
-    model = Model(view, file_name)
+    model = Model(view, file_path)
     controller: Controller = Controller(model)
     controller.run()
+
+
+def get_file_path():
+    if len(sys.argv) < 2:
+        home = str(Path.home())
+        path = os.path.join(home, 'Documents', 'newNote.nnn')
+        return add_unique_postfix(path)
+    return sys.argv[1]
+
+
+def add_unique_postfix(file_name):
+    if not os.path.exists(file_name):
+        return file_name
+    path, name = os.path.split(file_name)
+    name, ext = os.path.splitext(name)
+    for i in range(1, 1000000):
+        unique_filename = os.path.join(path, '%s(%d)%s' % (name, i, ext))
+        if not os.path.exists(unique_filename):
+            return unique_filename
+    raise Exception("Out of default file names")
 
 
 if __name__ == '__main__':
